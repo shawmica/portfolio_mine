cddf9f53814cd6f795f4a2c514876cdbde66033b
Shawmica Sivatharan <shawmi3030@gmail.com>
2025-05-17 20:58:15 +0530

    Your commit message here

--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -2,34 +2,55 @@ import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle }
 import { Button } from "@/components/ui/button";
 import { ExternalLink, Github } from "lucide-react";
 import { motion } from "framer-motion";
+import fungiImage from '../img/fungi.png';
+import slide from '../img/slide.jpg';
+import vol from '../img/vol.png';
 
 const projectsData = [
   {
     id: 1,
-    title: "E-commerce Platform",
-    description: "A full-featured online store with payment integration, user accounts, and inventory management.",
-    tags: ["React", "Node.js", "MongoDB", "Stripe"],
-    imageUrl: "https://images.unsplash.com/photo-1563986768609-322da13575f3?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80",
-    demoUrl: "https://example.com",
-    githubUrl: "https://github.com"
+    title: "Fungal Growth Analysis",
+    description: "A 14-day image processing study analyzing black fungi (Hyphae Penta) on coconut flesh using OpenCV and Python.",
+    tags: ["OpenCV", "Python", "Matplotlib", "ImageSegmentation"],
+    imageUrl: fungiImage,
+    demoUrl: "https://www.linkedin.com/posts/shawmica-sivatharan_fungalanalysis-imageprocessing-mycology-activity-7243471294531280896-GK-z?utm_source=share&utm_medium=member_desktop&rcm=ACoAAD4mIRgB1YfZferkQ68Aclke_nk76XKMsSA",
+    githubUrl: "https://github.com/shawmica/Image-Elites"
   },
   {
     id: 2,
-    title: "Task Management App",
-    description: "A productivity application for organizing tasks with team collaboration features.",
-    tags: ["Next.js", "TypeScript", "PostgreSQL", "Tailwind"],
-    imageUrl: "https://images.unsplash.com/photo-1484480974693-6ca0a78fb36b?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80",
-    demoUrl: "https://example.com",
-    githubUrl: "https://github.com"
+    title: "Gesture-Controlled Slides",
+    description: "Built a gesture-based slide tool using OpenCV and cvzone’s HandTrackingModule.",
+    tags: ["OpenCV", "CvZone", "Python", "HandTrackingModule", "Mediapipe", "ComputerVision"],
+    imageUrl: slide,
+    demoUrl: "https://www.linkedin.com/posts/shawmica-sivatharan_computervision-opencv-gesturecontrol-activity-7215255849957490688-UhvG?utm_source=share&utm_medium=member_desktop&rcm=ACoAAD4mIRgB1YfZferkQ68Aclke_nk76XKMsSA",
+    githubUrl: "https://github.com/shawmica/Presentation_with_Gestures"
   },
   {
     id: 3,
-    title: "Personal Finance Dashboard",
-    description: "A dashboard for tracking expenses, income, and investments with visualization tools.",
-    tags: ["React", "D3.js", "Firebase", "Material UI"],
-    imageUrl: "https://images.unsplash.com/photo-1511376777868-611b54f68947?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80",
-    demoUrl: "https://example.com",
-    githubUrl: "https://github.com"
+    title: "Gesture Volume Control",
+    description: "Developed a hand gesture-based volume system using OpenCV, Python, and Pycaw.",
+    tags: ["OpenCV", "Python", "Pycaw", "MediaPipe", "NumPy"],
+    imageUrl: vol,
+    demoUrl: "https://www.linkedin.com/posts/shawmica-sivatharan_python-opencv-computervision-activity-7207244949321310209-9vFs?utm_source=share&utm_medium=member_desktop&rcm=ACoAAD4mIRgB1YfZferkQ68Aclke_nk76XKMsSA",
+    githubUrl: "https://github.com/shawmica/HandGesture"
+  },
+  {
+    id: 4,
+    title: "Frame Focus",
+    description: "A dynamic photography portfolio with an interactive gallery and a contact form that lets visitors easily get in touch.",
+    tags: ["HTML", "CSS", "Javascript", "React"],
+    imageUrl: vol,
+    demoUrl: "https://www.linkedin.com/posts/shawmica-sivatharan_python-opencv-computervision-activity-7207244949321310209-9vFs?utm_source=share&utm_medium=member_desktop&rcm=ACoAAD4mIRgB1YfZferkQ68Aclke_nk76XKMsSA",
+    githubUrl: "https://github.com/shawmica/photography_website"
+  },
+  {
+    id: 4,
+    title: "Recipe Management Web Application ",
+    description: "A full-stack recipe management app, users can browse, search, and filter recipes in real-time, while admins manage recipes through a secure panel.",
+    tags: ["HTML", "CSS", "React", "Typescript","Python","Django"],
+    imageUrl: vol,
+    demoUrl: "https://www.linkedin.com/posts/shawmica-sivatharan_reactjs-typescript-django-activity-7327213614342328320-vmXh?utm_source=share&utm_medium=member_desktop&rcm=ACoAAD4mIRgB1YfZferkQ68Aclke_nk76XKMsSA",
+    githubUrl: "https://github.com/shawmica/chefs-corner"
   }
 ];
 
